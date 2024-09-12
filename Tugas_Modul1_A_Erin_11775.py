eb17e64928f6a6ad423a034e28270c863a443c14
eirine0425 <220711775@students.uajy.ac.id>
2024-09-12 14:39:58 +0700

    Update Tugas_Modul1_A_Erin_11775.py

--- Tugas_Modul1_A_Erin_11775.py
+++ Tugas_Modul1_A_Erin_11775.py
@@ -3,8 +3,8 @@ import pandas as pd
 import pickle
 import os
 
-model_directory = r'D:\Tugas_A_11775' 
-model_path = os.path.join(model_directory, 'rf_diabetes_model.pkl')
+
+model_path = 'rf_diabetes_model.pkl'
 
 if os.path.exists(model_path):
     try:
